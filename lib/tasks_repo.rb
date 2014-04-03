50aafc004c4a87e2bb13350f91231369377e46fd
Paul Wenig <pwenig@gmail.com>
2014-04-03 15:35:39 -0600

    refactored to remove duplicate database connection
    Signed-off-by: Aubrey Howell <simplyaubs@gmail.com>

--- lib/tasks_repo.rb
+++ lib/tasks_repo.rb
@@ -6,14 +6,14 @@ class TasksRepo
   end
 
   def insert(task)
-    db = Sequel.connect('postgres://gschool_user:password@localhost:5432/tasks_test')
-    tasks = db[:tasks]
+    @db
+    tasks = @db[:tasks]
     tasks.insert(task)
   end
 
   def select_all
-    db = Sequel.connect('postgres://gschool_user:password@localhost:5432/tasks_test')
-    tasks = db[:tasks]
+    @db
+    tasks = @db[:tasks]
     tasks.to_a
   end
 end
